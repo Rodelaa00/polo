39be9a48ac3b998b3a376ac22f90c89cc98d28ec
Rodela <rodela448@gmail.com>
2018-04-23 12:40:10 +0600

    first

---
+++ src/com/polo/serv/PoloServlet.java
@@ -0,0 +1,14 @@
+package com.polo.serv;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "PoloServlet")
+public class PoloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
